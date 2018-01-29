d778f43d7220f4d2a6a5230b7edf4464b1965ff0
oilgraphy <oilgraphy.com@gmail.com>
2018-01-29 00:13:29 -0800

    list posts

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import promise from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
